e8c8d36c3bf31efdc792a0b23ce9e5ed3a501c1b
Bonifaac <80424382+Bonifaac@users.noreply.github.com>
2021-04-16 21:28:07 +0200

    Add files via upload

--- php/regisztracio.php
+++ php/regisztracio.php
@@ -1,5 +1,11 @@
 <?php
     session_start();
+    $emailErr=false;
+    $fnevErr=false;
+    $jelszoErr=false;
+    $jelszoErr2=false;
+    $ures=false;
+    $sikeres=false;
     include "../php/kozos.php";
     if(isset($_SESSION["user"]) && !empty($_SESSION["user"])) {
         header("Location: ../php/profil.php");
@@ -36,103 +42,102 @@
 </header>
 
 <main>
+    <?php
+    $fiokok = loaduser("../user/user.txt");
+
+    $errors = [];
+    $email = "";
+    $userName = "";
+    $firstName = "";
+    $lastName = "";
+    $jelszo = "";
+    $jelszo2 = "";
+    $profilPicture = "";
+
+    if(isset($_POST["regisztracio"])) {
+        $email = $_POST["email"];
+        $userName = $_POST["userName"];
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $jelszo = $_POST["jelszo"];
+        $jelszo2 = $_POST["jelszo2"];
+        $profilPicture = "../user/" . $_POST["profilPicture"];
+
+        foreach ($fiokok as $fiok) {
+            if($fiok["email"] === $email) {
+                $emailErr=true;
+            }
+
+            if($fiok["userName"] === $userName){
+                $fnevErr=true;
+            }
+        }
+
+        if(strlen($jelszo) < 5) {
+            $fnevErr=true;
+        }
+
+        if(!preg_match('/[A-Za-z]/',$jelszo) || !preg_match('/[0-9]/',$jelszo)){
+            $jelszoErr=true;
+        }
+
+        if($jelszo !== $jelszo2) {
+            $jelszoErr2=true;
+        }
+
+
+
+        if(!$emailErr && !$jelszoErr && !$fnevErr && !$jelszoErr2 && !$ures){
+
+            $data = [
+                "email"=>$email,
+                "userName"=>$userName,
+                "firstName"=>$firstName,
+                "lastName"=>$lastName,
+                "jelszo"=>$jelszo,
+                "profilPicture"=>$profilPicture
+            ];
+            $sikeres=true;
+            saveuser("../user/user.txt", $data);
+        } else {
+            if(empty($_POST["email"]) || empty($_POST["userName"]) || empty($_POST["firstName"]) || empty($_POST["lastName"]) ||
+                empty($_POST["jelszo"]) || empty($_POST["jelszo"])){
+                $ures=true;
+            }
+        }
+
+    }
+    ?>
     <div class="loginpage">
         <form action="regisztracio.php" method="post" enctype="multipart/form-data" autocomplete="off">
             <div>
+                <?php if(isset($_POST["regisztracio"]) && $sikeres) : ?> <h2>Sikeres regisztráció!</h2><?php endif; ?>
+                <?php if(isset($_POST["regisztracio"]) && $ures) : ?> <div class="alert" >Minden mezőt kikell tölteni!</div><?php endif; ?>
                 <label><input class="inputbox" type="email" name="email" placeholder="email@email.hu"></label><br>
-                <div>Hali</div>
+                <?php if(isset($_POST["regisztracio"]) && $emailErr) : ?> <div class="alert">Ezzel az emaillel már van regisztrált fiók!</div><?php endif; ?>
             </div>
             <div>
                 <label><input class="inputbox" type="text" name="userName" placeholder="Felhasználónév"></label><br>
-                <div>Hali</div>
+                <?php if(isset($_POST["regisztracio"]) && $fnevErr) : ?> <div class="alert">A felhasználónév már foglalt!</div><?php endif; ?>
             </div>
             <div>
                 <label><input class="inputbox" type="text" name="firstName" placeholder="Vezetéknév"></label>
-                <div>Hali</div>
             </div>
             <div>
                 <label><input class="inputbox" type="text" name="lastName" placeholder="Keresztnév"></label><br>
-                <div>Hali</div>
             </div>
             <div>
                 <label><input class="inputbox" type="password" name="jelszo" placeholder="Jelszó"></label><br>
-                <div>Hali</div>
+                <?php if(isset($_POST["regisztracio"]) && $jelszoErr) : ?> <div class="alert">A jelszónak legalább 6 hosszúnak kell lennie, és számot és betűt is tartalmaznia kell!</div><?php endif; ?>
             </div>
             <div>
                 <label><input class="inputbox" type="password" name="jelszo2" placeholder="Jelszó újra"></label><br>
-                <div>Hali</div>
+                <?php if(isset($_POST["regisztracio"]) && $jelszoErr) : ?> <div class="alert">A két jelszó nem egyezik!</div><?php endif; ?>
             </div>
             <label><input hidden type="hidden" name="profilPicture" value="default.jpg"></label>
             <label><input class="headerbutton" type="submit" name="regisztracio" value="Regisztráció"></label>
         </form>
     </div>
-    <?php
-        $fiokok = loaduser("../user/user.txt");
-
-        $errors = [];
-        $email = "";
-        $userName = "";
-        $firstName = "";
-        $lastName = "";
-        $jelszo = "";
-        $jelszo2 = "";
-        $profilPicture = "";
-
-        if(isset($_POST["regisztracio"])) {
-            $email = $_POST["email"];
-            $userName = $_POST["userName"];
-            $firstName = $_POST["firstName"];
-            $lastName = $_POST["lastName"];
-            $jelszo = $_POST["jelszo"];
-            $jelszo2 = $_POST["jelszo2"];
-            $profilPicture = "../user/" . $_POST["profilPicture"];
-
-            foreach ($fiokok as $fiok) {
-                if($fiok["email"] === $email) {
-                    $errors[] = "Ezzel az emaillel már van regisztrált fiók!";
-                }
-
-                if($fiok["userName"] === $userName){
-                    $errors[] = "A felhasználónév már foglalt!";
-                }
-            }
-
-            if(strlen($jelszo) < 5) {
-                $errors[] = "A jelszó túl rövid!";
-            }
-
-            if(!preg_match('/[A-Za-z]/',$jelszo) || !preg_match('/[0-9]/',$jelszo)){
-                $errors[] = "A jelszónak betűt és számot is tartalmaznia kell!";
-            }
-
-            if($jelszo !== $jelszo2) {
-                $errors[] = "A két jelszó nem egyezik!";
-            }
-
-            if(empty($_POST["email"]) || empty($_POST["userName"]) || empty($_POST["firstName"]) || empty($_POST["lastName"]) ||
-                empty($_POST["jelszo"]) || empty($_POST["jelszo"])){
-                $errors = "Minden mezőt kikell tölteni!";
-            }
-
-            if(empty($errors)){
-                $data = [
-                    "email"=>$email,
-                    "userName"=>$userName,
-                    "firstName"=>$firstName,
-                    "lastName"=>$lastName,
-                    "jelszo"=>$jelszo,
-                    "profilPicture"=>$profilPicture
-                ];
-
-                saveuser("../user/user.txt", $data);
-            } else {
-                foreach ($errors as $err) {
-                    echo $err . "<br>";
-                }
-            }
-
-        }
-    ?>
 </main>
 <?php
 include "../php/footer.php";
